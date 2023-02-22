7e095d7ee280d1574cd59d207ea074a9b079abbb
DSorbon <davlatshoev.s@mail.ru>
2023-02-22 21:18:21 +0500

    added price calculation for parking

--- app/Http/Controllers/Api/V1/ParkingController.php
+++ app/Http/Controllers/Api/V1/ParkingController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\StartParkingRequest;
 use App\Http\Resources\Api\V1\ParkingResource;
 use App\Models\Parking;
+use App\Services\ParkingPriceService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -27,7 +28,8 @@ class ParkingController extends Controller
     public function stop(Parking $parking): Response|JsonResponse
     {
         $parking->update([
-            'end_time' => now()
+            'end_time' => now(),
+            'total_price' => ParkingPriceService::calculatePrice($parking->zone_id, $parking->start_time)
         ]);
 
         return $this->sendResponse(ParkingResource::make($parking), '', Response::HTTP_ACCEPTED);
--- app/Http/Resources/Api/V1/ParkingResource.php
+++ app/Http/Resources/Api/V1/ParkingResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api\V1;
 
+use App\Services\ParkingPriceService;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,13 +15,19 @@ class ParkingResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $totalPrice = $this->total_price ?? ParkingPriceService::calculatePrice(
+            $this->zone_id,
+            $this->start_time,
+            $this->end_time
+        );
+
         return [
             'id' => $this->id,
             'zone' => ZoneResource::make($this->zone),
             'vehicle' => VehicleResource::make($this->vehicle),
             'start_time' => $this->start_time->toDateTimeString(),
             'end_time' => $this->end_time?->toDateTimeString(),
-            'total_price' => $this->total_price
+            'total_price' => $totalPrice
         ];
     }
 }
